4e01e05ecae081855b5db860614eeccc5829b161
himanshu-skrpkvnp <himanshusinghskrpkvnp@gmail.com>
2024-04-22 12:18:38 +0530

    joke_api added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { JokesService } from './jokes.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'openapi_angular';
+  joke : String = "" ;
+  constructor( private jokeService : JokesService ){
+
+  }
+  fetchJoke(): void {
+    this.jokeService.getJoke().subscribe((data: any) => {
+      this.joke = data.joke;
+    });
+  }
 }
