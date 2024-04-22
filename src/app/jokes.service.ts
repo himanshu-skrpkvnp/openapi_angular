d2c5f473fcf91b0ed96900afce566a81fefccae6
himanshu-skrpkvnp <himanshusinghskrpkvnp@gmail.com>
2024-04-22 13:53:02 +0530

    api added

--- src/app/jokes.service.ts
+++ src/app/jokes.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,12 @@ import { Observable } from 'rxjs';
 export class JokesService {
    
   url = 'https://icanhazdadjoke.com' ;
+  apiKey = environment.apiKey ;
   constructor( private http : HttpClient ) {}
   getJoke() : Observable <any> {
+    const headers = new HttpHeaders()
+      .set('Accept', 'application/json');
 
-    return this.http.get( this.url  , { headers: { Accept: 'application/json' } }) ;
+    return this.http.get( this.url  , { headers}) ;
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiKey: '710aad5e87msh11210aa01027da0p1f86dbjsn7b87e799b40c' , 
+  };
